d8b8722c6e1f8804fbe70b13317127fded56c32c
unknown <nicole.gomez@ucb.edu.bo>
2025-03-06 09:42:11 -0400

    feat: si jugador 1 va 2 - 0 se muestra 30 - 0

--- src/Tennis.js
+++ src/Tennis.js
@@ -2,7 +2,12 @@ function tennisScore(player1Score, player2Score) {
     if (player1Score === 1 && player2Score === 0) {
         return "15 - 0";
     }
+
+    if (player1Score === 2 && player2Score === 0) {
+        return "30 - 0";
+    }
     return "love - love"; 
 }
 
 export default tennisScore;
+
