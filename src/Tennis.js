cefbd3163433f5b0c60f89760ab16f23aea4acb6
JaimeTadeo <deugartebutron@gmail.com>
2025-03-06 09:45:54 -0400

    feat:muestra 40 - 0 si un jgador anota

--- src/Tennis.js
+++ src/Tennis.js
@@ -2,12 +2,13 @@ function tennisScore(player1Score, player2Score) {
     if (player1Score === 1 && player2Score === 0) {
         return "15 - 0";
     }
-
     if (player1Score === 2 && player2Score === 0) {
         return "30 - 0";
+    }
+    if (player1Score === 3 && player2Score === 0) {
+        return "40 - 0";
     }
     return "love - love"; 
 }
 
 export default tennisScore;
-
--- src/Tennis.test.js
+++ src/Tennis.test.js
@@ -9,7 +9,11 @@ describe("Tennis Score", () => {
         expect(tennisScore(1, 0)).toEqual("15 - 0");
     });
 
-    it("debería mostrar 30 - 0 cuando el jugador 1 tiene 2 punto y el jugador 2 tiene 0", () => {
+    it("debería mostrar 30 - 0 cuando el jugador 1 tiene 2 puntos y el jugador 2 tiene 0", () => {
         expect(tennisScore(2, 0)).toEqual("30 - 0");
+    });
+
+    it("debería mostrar 40 - 0 cuando el jugador 1 tiene 3 puntos y el jugador 2 tiene 0", () => {
+        expect(tennisScore(3, 0)).toEqual("40 - 0");
     });
 });
